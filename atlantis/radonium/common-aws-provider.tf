f1a80275e7f5231c8c0986ffa4358ba31962e7fd
Carmen Navarrete Hernandez <carmennavarreteh@gmail.com>
2023-05-03 20:54:52 +0200

    PLE-13502 test: Test Post workflow

--- atlantis/radonium/common-aws-provider.tf
+++ atlantis/radonium/common-aws-provider.tf
@@ -1,25 +1,6 @@
 provider "aws" {
-  region = var.aws_region
-  allowed_account_ids = [
-    var.aws_account_id
-  ]
+  region = "eu-east"
 
-  # assume_role {
-  #   role_arn     = "arn:aws:iam::${local.account_id}:role/${local.control_plane_account_access_role}"
-  #   session_name = var.sts_session_name
-  # }
 
-  default_tags {
-    tags = merge(
-      var.extra_tags,
-      {
-        created_by = var.created_by
-        owner      = var.owner
-      }
-    )
-  }
 
-  ignore_tags {
-    keys = ["created_by"]
-  }
 }
